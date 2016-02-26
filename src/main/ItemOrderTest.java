4c3f776c0eadd463916a35ec983cce35095125cf
George <georgeormrod@gmail.com>
2016-02-26 12:18:17 +0000

    Test Classes Made

--- src/main/ItemOrderTest.java
+++ src/main/ItemOrderTest.java
@@ -7,68 +7,59 @@ import org.junit.Test;
 public class ItemOrderTest {
 
 	@Test
-	public void testItemOrderStringDoubleInt() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		
+		ItemOrder i = new ItemOrder("Apple",12,2);
+		ItemOrder i2 = new ItemOrder("Apple",12);
+		
+		assertEquals("getName() should be Apple","Apple",i.getProductName());
+		assertEquals("getName() should be equal to Apple","Apple", i2.getProductName());
+	
+	}
+	
+	@Test
+	public void testAddItem()
+	{
+		ItemOrder i = new ItemOrder("Apple",12);
+		
+		int i2 = i.addItem();
+		
+		assertEquals("The ItemOrder Should have a Quantity of 2",2,i2);
+		
+	}
+	
+	@Test
+	public void testRemoveItem()
+	{
+		ItemOrder i = new ItemOrder("Apple",12,2);
+		
+		int i2 = i.removeItem();
+		
+		assertEquals("The ItemOrder should have a quantity of 1",1,i2);
+		
+	}
+	
+	@Test
+	public void testAddMoreThanOneItem()
+	{
+		ItemOrder i = new ItemOrder("Apple",12,3);
+		
+		int i2 = i.addItems(2);
+		
+		assertEquals("The ItemOrder should have a quantity of 5",5,i2);
+		
+	}
+	
+	@Test
+	public void testRemoveMoreThanOneItem()
+	{
+		ItemOrder i = new ItemOrder ("Apple",12,5);
+		
+		int i2 = i.removeItems(2);
+		
+		assertEquals("The ItemOrder should have a quantity of 3",3,i2);
+		
 	}
 
-	@Test
-	public void testItemOrderStringDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetProductName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetLatestPrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetQuantity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTotalOrder() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddItemsDoubleInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddItemsInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEditItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
 
 }
--- src/main/ShoppingBasketTest.java
+++ src/main/ShoppingBasketTest.java
@@ -7,83 +7,15 @@ import org.junit.Test;
 public class ShoppingBasketTest {
 
 	@Test
-	public void testShoppingBasket() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		
+		ShoppingBasket sb = new ShoppingBasket();
+		
+		int i = sb.getItems().size();
+		
+		
 	}
-
-	@Test
-	public void testAddProductStringDoubleInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddProductStringDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEditItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveProductStringInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveProductString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testClearBasket() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testOrderItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNumberOfProducts() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBasketTotal() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNumberOfItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCurrentPrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsProductInTheBasket() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSaveBasket() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFindItemByName() {
-		fail("Not yet implemented");
-	}
+	
+	
 
 }
